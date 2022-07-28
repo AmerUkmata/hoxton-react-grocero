d0d22d7c1767a62b98ccf3b4eaf7ca9fe97f6d82
Animechill30 <82389963+AmerUkmata@users.noreply.github.com>
2022-07-28 19:08:01 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,53 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div><header id="store">
+      <h1>Grocero</h1>
+      <ul className="item-list store--item-list">
+        <li>
+          <div className="store--item-icon">
+            <img src="assets/icons/001-beetroot.svg" alt="beetroot" />
+          </div>
+          <button>Add to cart</button>
+        </li>
+      </ul>
+    </header>
+
+      <main id="cart">
+        <h2>Your Cart</h2>
+
+        <div className="cart--item-list-container">
+          <ul className="item-list cart--item-list">
+            <li>
+              <img
+                className="cart--item-icon"
+                src="assets/icons/001-beetroot.svg"
+                alt="beetroot"
+              />
+              <p>beetroot</p>
+              <button className="quantity-btn remove-btn center">-</button>
+              <span className="quantity-text center">1</span>
+              <button className="quantity-btn add-btn center">+</button>
+            </li>
+          </ul>
+        </div>
+
+        <div className="total-section">
+          <div>
+            <h3>Total</h3>
+          </div>
+
+          <div>
+            <span className="total-number">£0.00</span>
+          </div>
+        </div>
+      </main>
+    </div>
+  )
+}
+
+export default App
